fa439de6da0b8f5e1cae6362fe607ce77d30d186
Christopher Stebe <chris@stebe.eu>
2013-08-12 16:12:05 +0200

    code formatting model

--- models/Slit.php
+++ models/Slit.php
@@ -6,51 +6,44 @@ Yii::import('Slit.*');
 
 class Slit extends BaseSlit
 {
-	// Add your model-specific methods here. This file will not be overriden by gtc except you force it.
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
 
-	public function init()
-	{
-		return parent::init();
-	}
+    // Add your model-specific methods here. This file will not be overriden by gtc except you force it.
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
 
-	public function get_label() {
-		return (string) $this->status;
+    public function init()
+    {
+        return parent::init();
+    }
 
-	}
+    public function get_label()
+    {
+        return (string) $this->status;
+    }
 
-	public function behaviors()
-	{
-		return array_merge(
-			parent::behaviors(),
-			array(
-		'CTimestampBehavior' => array(
-				'class' => 'zii.behaviors.CTimestampBehavior',
-				'createAttribute' => 'created_at',
-				'updateAttribute' => 'updated_at',
-					),
+    public function behaviors()
+    {
+        return array_merge(
+                parent::behaviors(), array(
+            'CTimestampBehavior' => array(
+                'class' => 'zii.behaviors.CTimestampBehavior',
+                'createAttribute' => 'created_at',
+                'updateAttribute' => 'updated_at',
+            ),
+            'OwnerBehavior' => array(
+                'class' => 'OwnerBehavior',
+                'ownerColumn' => 'created_by',
+            ),
+        ));
+    }
 
-		'OwnerBehavior' => array(
-								'class' => 'OwnerBehavior',
-							'ownerColumn' => 'created_by',
-								),
-
-            ));
-	}
-
-
-	public function rules()
-	{
-		return array_merge(
-		    parent::rules()
-            /*, array(
-			array('column1, column2', 'rule1'),
-			array('column3', 'rule2'),
-		    )*/
-		);
-	}
+    public function rules()
+    {
+        return array_merge(
+                parent::rules()
+        );
+    }
 
 }
