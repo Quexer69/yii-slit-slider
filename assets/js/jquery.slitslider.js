98b30018f7f6d35d735df60d36066ef45eb4bd24
Christopher Stebe <chris@stebe.eu>
2013-08-20 22:51:28 +0200

    activate some effects

--- assets/js/jquery.slitslider.js
+++ assets/js/jquery.slitslider.js
@@ -68,21 +68,21 @@
 
 	$.Slitslider.defaults = {
 		// transitions speed
-		speed : 800,
+		speed : 1400,
 		// if true the item's slices will also animate the opacity value
-		optOpacity : false,
+		optOpacity : true,
 		// amount (%) to translate both slices - adjust as necessary
-		translateFactor : 230,
+		translateFactor : 40,
 		// maximum possible angle
 		maxAngle : 25,
 		// maximum possible scale
-		maxScale : 2,
+		maxScale : 3,
 		// slideshow on / off
-		autoplay : false,
+		autoplay : true,
 		// keyboard navigation
 		keyboard : true,
 		// time between transitions
-		interval : 4000,
+		interval : 6000,
 		// callbacks
 		onBeforeChange : function( slide, idx ) { return false; },
 		onAfterChange : function( slide, idx ) { return false; }
