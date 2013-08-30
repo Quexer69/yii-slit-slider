50f34bc1ac207f88be9ef8a8cf3e0e31628ba1b3
Christopher Stebe <chris@stebe.eu>
2013-08-31 00:13:43 +0200

    update cruds

--- views/default/admin.php
+++ views/default/admin.php
@@ -1,7 +1,6 @@
 <?php
 $this->breadcrumbs[] = Yii::t('SlitSliderModule.crud', 'Slits');
 
-
 Yii::app()->clientScript->registerScript('search', "
 $('.search-button').click(function(){
 $('.search-form').toggle();
@@ -16,25 +15,24 @@ return false;
 ");
 ?>
 <?php $this->widget("TbBreadcrumbs", array("links" => $this->breadcrumbs)) ?>
-<h1>
-    <?php echo Yii::t('SlitSliderModule.crud', 'Slits'); ?> <small><?php echo Yii::t('SlitSliderModule.crud', 'Manage'); ?></small>
-</h1>
+
+<h1><?php echo Yii::t('SlitSliderModule.crud', 'Slits'); ?> <small><?php echo Yii::t('SlitSliderModule.crud', 'Manage'); ?></small></h1>
 
 <?php $this->renderPartial("_toolbar", array("model" => $model)); ?>
 <br />
 <?php
 $this->widget('TbGridView', array(
-    'id' => 'slit-grid',
-    'dataProvider' => $model->search(),
-    'filter' => $model,
-    'pager' => array(
-        'class' => 'TbPager',
-        'displayFirstAndLast' => true,
+    'id'            => 'slit-grid',
+    'dataProvider'  => $model->search(),
+    'filter'        => $model,
+    'pager'         => array(
+                        'class' => 'TbPager',
+                        'displayFirstAndLast' => true,
     ),
     'columns' => array(
         array(
-            'name' => 'media_id',
-            'type' => 'raw',
+            'name'  => 'media_id',
+            'type'  => 'raw',
             'value' => function($data) {
                 return ($data->media_id !== null) 
                         ? CHtml::link($data->media->image(SlitSliderWidget::imagePreset_admin), array('//slitSlider/default/view?id=' . $data->id), array('class' => 'pull-left btn-info'))
@@ -44,49 +42,40 @@ $this->widget('TbGridView', array(
         'type',
         array(
             'class' => 'editable.EditableColumn',
-            'name' => 'headline',
-            'editable' => array(
-                'url' => $this->createUrl('/slitSlider/default/editableSaver'),
-            )
+            'name'  => 'headline',
+            'editable' => array('url'   => $this->createUrl('/slitSlider/default/editableSaver'))
         ),
         array(
             'class' => 'editable.EditableColumn',
-            'name' => 'group_id',
-            'editable' => array(
-                'url' => $this->createUrl('/slitSlider/default/editableSaver'),
-            )
+            'name'  => 'group_id',
+            'editable' => array('url'   => $this->createUrl('/slitSlider/default/editableSaver'))
         ),
         array(
             'class' => 'editable.EditableColumn',
-            'name' => 'rank',
-            'editable' => array(
-                'url' => $this->createUrl('/slitSlider/default/editableSaver'),
-            )
+            'name'  => 'rank',
+            'editable' => array('url'   => $this->createUrl('/slitSlider/default/editableSaver'))
         ),
         array(
             'class' => 'editable.EditableColumn',
-            'name' => 'status',
-            'editable' => array(
-                'url' => $this->createUrl('/slitSlider/default/editableSaver'),
-                'type' => 'select',
-                'source' => array(
-                    'hidden' => 'hidden',
-                    'published' => 'published',
-                ),
-            )
+            'name'  => 'status',
+            'editable' => array('url'   => $this->createUrl('/slitSlider/default/editableSaver'),
+                                'type'  => 'select',
+                                'source' => array(
+                                    'hidden' => 'hidden',
+                                    'published' => 'published',
+                                ),
+                            )
         ),
         array(
             'class' => 'editable.EditableColumn',
-            'name' => 'language',
-            'editable' => array(
-                'url' => $this->createUrl('/slitSlider/default/editableSaver'),
-            )
+            'name'  => 'language',
+            'editable' => array('url'   => $this->createUrl('/slitSlider/default/editableSaver'))
         ),
         array(
             'class' => 'TbButtonColumn',
-            'viewButtonUrl' => "Yii::app()->controller->createUrl('view', array('id' => \$data->id))",
-            'updateButtonUrl' => "Yii::app()->controller->createUrl('update', array('id' => \$data->id))",
-            'deleteButtonUrl' => "Yii::app()->controller->createUrl('delete', array('id' => \$data->id))",
+            'viewButtonUrl'             => "Yii::app()->controller->createUrl('view', array('id' => \$data->id))",
+            'updateButtonUrl'           => "Yii::app()->controller->createUrl('update', array('id' => \$data->id))",
+            'deleteButtonUrl'           => "Yii::app()->controller->createUrl('delete', array('id' => \$data->id))",
         ),
     ),
 ));
--- views/default/view.php
+++ views/default/view.php
@@ -20,15 +20,15 @@ $this->breadcrumbs[] = $model->id;
             'attributes' => array(
                 #'id',
                 array(
-                    'name' => 'type',
-                    'type' => 'html',
+                    'name'  => 'type',
+                    'type'  => 'html',
                     'value' => ($model->type === 'image') 
                                 ? '<span class="badge"><i class="icon-camera"></i> ' . $model->type . '</span>' 
                                 : '<span class="badge"><i class="icon-barcode"></i> ' . $model->type . '</span>'
                 ),
                 array(
-                    'name' => 'status',
-                    'type' => 'raw',
+                    'name'  => 'status',
+                    'type'  => 'raw',
                     'value' => $this->widget('EditableField', array(
                                         'model' => $model,
                                         'emptytext' => 'Click to select',
@@ -47,8 +47,8 @@ $this->breadcrumbs[] = $model->id;
                                 )
                 ),
                 array(
-                    'name' => 'image_preset',
-                    'type' => 'raw',
+                    'name'  => 'image_preset',
+                    'type'  => 'raw',
                     'value' => $this->widget('EditableField', array(
                                         'model' => $model,
                                         'emptytext' => 'Click to select',
@@ -64,15 +64,15 @@ $this->breadcrumbs[] = $model->id;
                                 )
                 ),
                 array(
-                    'name' => 'media_id',
-                    'type' => 'raw',
+                    'name'  => 'media_id',
+                    'type'  => 'raw',
                     'value' => ($model->media_id !== null) 
-                            ? CHtml::link($model->media->image(SlitSliderWidget::imagePreset_view), array('//p3media/p3Media/view?id=' . $model->media_id), array('class' => 'pull-left btn-info'))
-                            : '<i class="icon-ban-circle"></i>',
+                                ? CHtml::link($model->media->image(SlitSliderWidget::imagePreset_view), array('//p3media/p3Media/view?id=' . $model->media_id), array('class' => 'pull-left btn-info'))
+                                : '<i class="icon-ban-circle"></i>',
                 ),
                 array(
-                    'name' => 'group_id',
-                    'type' => 'raw',
+                    'name'  => 'group_id',
+                    'type'  => 'raw',
                     'value' => $this->widget('EditableField', array(
                                         'model' => $model,
                                         'attribute' => 'group_id',
@@ -81,8 +81,8 @@ $this->breadcrumbs[] = $model->id;
                                 )
                 ),
                 array(
-                    'name' => 'rank',
-                    'type' => 'raw',
+                    'name'  => 'rank',
+                    'type'  => 'raw',
                     'value' => $this->widget('EditableField', array(
                                         'model' => $model,
                                         'attribute' => 'rank',
@@ -91,8 +91,8 @@ $this->breadcrumbs[] = $model->id;
                                 )
                 ),
                 array(
-                    'name' => 'language',
-                    'type' => 'raw',
+                    'name'  => 'language',
+                    'type'  => 'raw',
                     'value' => $this->widget('EditableField', array(
                                         'model' => $model,
                                         'attribute' => 'language',
@@ -101,8 +101,8 @@ $this->breadcrumbs[] = $model->id;
                                 )
                 ),
                 array(
-                    'name' => 'headline',
-                    'type' => 'raw',
+                    'name'  => 'headline',
+                    'type'  => 'raw',
                     'value' => ($model->headline !== null) 
                                 ? $this->widget('EditableField', array(
                                         'model' => $model,
@@ -113,8 +113,8 @@ $this->breadcrumbs[] = $model->id;
                                 : '<i class="icon-ban-circle"></i>'
                 ),
                 array(
-                    'name' => 'subline',
-                    'type' => 'raw',
+                    'name'  => 'subline',
+                    'type'  => 'raw',
                     'value' => ($model->subline !== null) 
                                 ? $this->widget('EditableField', array(
                                         'model' => $model,
@@ -125,8 +125,8 @@ $this->breadcrumbs[] = $model->id;
                                 : '<i class="icon-ban-circle"></i>'
                 ),
                 array(
-                    'name' => 'link',
-                    'type' => 'raw',
+                    'name'  => 'link',
+                    'type'  => 'raw',
                     'value' => ($model->link !== null) 
                                 ? $this->widget('EditableField', array(
                                         'model' => $model,
@@ -137,63 +137,63 @@ $this->breadcrumbs[] = $model->id;
                                 : '<i class="icon-ban-circle"></i>'
                 ),
                 array(
-                    'name' => 'body_html',
-                    'type' => 'raw',
+                    'name'  => 'body_html',
+                    'type'  => 'raw',
                     'value' => ($model->body_html !== null) ? '<pre>' . $model->body_html . '</pre>' : '<i class="icon-ban-circle"></i>'
                 ),
                 array(
-                    'name' => 'data_orientation',
-                    'type' => 'raw',
+                    'name'  => 'data_orientation',
+                    'type'  => 'raw',
                     'value' => ($model->data_orientation !== null) ? $model->data_orientation : '<i class="icon-ban-circle"></i>'
                 ),
                 array(
-                    'name' => 'data_slice1_rotation',
-                    'type' => 'raw',
+                    'name'  => 'data_slice1_rotation',
+                    'type'  => 'raw',
                     'value' => ($model->data_slice1_rotation !== null) ? $model->data_slice1_rotation : '<i class="icon-ban-circle"></i>'
                 ),
                 array(
-                    'name' => 'data_slice2_rotation',
-                    'type' => 'raw',
+                    'name'  => 'data_slice2_rotation',
+                    'type'  => 'raw',
                     'value' => ($model->data_slice2_rotation !== null) ? $model->data_slice2_rotation : '<i class="icon-ban-circle"></i>'
                 ),
                 array(
-                    'name' => 'data_slice1_scale',
-                    'type' => 'raw',
+                    'name'  => 'data_slice1_scale',
+                    'type'  => 'raw',
                     'value' => ($model->data_slice1_scale !== null) ? $model->data_slice1_scale : '<i class="icon-ban-circle"></i>'
                 ),
                 array(
-                    'name' => 'data_slice2_scale',
-                    'type' => 'raw',
+                    'name'  => 'data_slice2_scale',
+                    'type'  => 'raw',
                     'value' => ($model->data_slice2_scale !== null) ? $model->data_slice2_scale : '<i class="icon-ban-circle"></i>'
                 ),
                 array(
-                    'name' => 'start_date',
-                    'type' => 'raw',
+                    'name'  => 'start_date',
+                    'type'  => 'raw',
                     'value' => ($model->start_date !== null) ? $model->start_date : '<i class="icon-ban-circle"></i>'
                 ),
                 array(
-                    'name' => 'end_date',
-                    'type' => 'raw',
+                    'name'  => 'end_date',
+                    'type'  => 'raw',
                     'value' => ($model->end_date !== null) ? $model->end_date : '<i class="icon-ban-circle"></i>'
                 ),
                 array(
-                    'name' => 'created_at',
-                    'type' => 'raw',
+                    'name'  => 'created_at',
+                    'type'  => 'raw',
                     'value' => ($model->created_at !== null) ? $model->created_at : '<i class="icon-ban-circle"></i>'
                 ),
                 array(
-                    'name' => 'updated_at',
-                    'type' => 'raw',
+                    'name'  => 'updated_at',
+                    'type'  => 'raw',
                     'value' => ($model->updated_at !== null) ? $model->updated_at : '<i class="icon-ban-circle"></i>'
                 ),
                 array(
-                    'name' => 'created_by',
-                    'type' => 'text',
+                    'name'  => 'created_by',
+                    'type'  => 'text',
                     'value' => $model->created_by
                 ),
                 array(
-                    'name' => 'updated_by',
-                    'type' => 'text',
+                    'name'  => 'updated_by',
+                    'type'  => 'text',
                     'value' => $model->updated_by
                 ),
             )
