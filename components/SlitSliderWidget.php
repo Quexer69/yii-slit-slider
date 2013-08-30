8e7c4c19ab36ab0cd3627f00dea9bdcdb07b707c
Christopher Stebe <chris@stebe.eu>
2013-08-30 22:44:43 +0200

    add Widget const

--- components/SlitSliderWidget.php
+++ components/SlitSliderWidget.php
@@ -40,6 +40,7 @@ class SlitSliderWidget extends CWidget
     const widgetName            = 'slitSlider';
     const SLIT_ACTIVE           = 'published';
     const imagePreset_view      = 'small-picture-crop-16-9';
+    const imagePreset_admin     = 'p3media-upload';
     const IMAGE                 = 'image';
     const HTML                  = 'html';
 
